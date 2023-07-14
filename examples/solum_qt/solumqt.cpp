743c884452c0012f5f9e2f77f4cca497df006d95
Claudio Hoffmann <c.hoffmann@dev-threads.de>
2023-07-14 19:00:40 +0200

    Qt: TCP: limit selectable probes to certified ones
    Solum requires the correct probe to be set via solumLoadApplication()
    for image acquisition to work.

--- examples/solum_qt/solumqt.cpp
+++ examples/solum_qt/solumqt.cpp
@@ -71,6 +71,7 @@ Solum::Solum(QWidget *parent) : QMainWindow(parent), connected_(false), imaging_
         }
 
         certified_.clear();
+        QStringList probeModels;
         auto json = doc.object();
         if (json.contains("results") && json["results"].isArray())
         {
@@ -85,13 +86,25 @@ Solum::Solum(QWidget *parent) : QMainWindow(parent), connected_(false), imaging_
                     {
                         auto serial = device["serial"].toString();
                         if (!serial.isEmpty())
+                        {
                             certified_[serial] = probe["crt"].toString();
+
+                            if (device.contains("model"))
+                            {
+                                auto model = device["model"].toString();
+                                if (!model.isEmpty())
+                                    if (probesSupported_.contains(model))
+                                        if (!probeModels.contains(model))
+                                            probeModels.push_back(model);
+                            }
+                        }
                     }
                 }
             }
 
             addStatus(tr("(Cloud) Found %1 valid OEM probes").arg(certified_.size()));
             ui_.tcp->setEnabled(true);
+            loadProbes(probeModels);
         }
     });
 
@@ -363,7 +376,7 @@ bool Solum::event(QEvent *event)
     {
         auto evt = static_cast<event::List*>(event);
         if (evt->probes_)
-            loadProbes(evt->list_);
+            probesSupported_ = evt->list_;
         else
             loadApplications(evt->list_);
         return true;
